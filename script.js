c997a1bcd3906e353cc7a017616adad78d004abe
shadz22 <shadz22@yahoo.co.uk>
2020-09-21 16:16:50 +0100

    Create Modules to put the code in

---
+++ script.js
@@ -0,0 +1,17 @@
+//******* Quiz Controller *********** 
+var quizController = (function() {
+
+
+})();
+
+//******* UI Controller ********
+var UIController = (function() {
+
+
+})();
+
+//******** Controller *********
+var controller = (function(quizCtrl, UICtrl) {
+
+
+})(quizController, UIController);
